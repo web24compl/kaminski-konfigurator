fcdcc5a71ec93538d56d47f59494f05fdf1f6845
lukasz <lukaszcysewski123@gmail.com>
2024-02-28 16:19:31 +0100

    fix yarn build images

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer__head">
         <a href="{{route('home')}}" >
-            <img src="{{ Vite::asset('resources/assets/logoAcademy.svg') }}" class="header__image">
+            <img src="{{ asset('assets/logoAcademy.svg') }}" class="header__image">
         </a>
         <div class="footer__links">
             <a href="/" target="_blank">Informacje Prawne</a>
@@ -13,7 +13,7 @@
         <span>WSZELKIE PRAWA ZASTRZEŻONE  © <a href="/">{{config('app.name')}} {{now()->format('Y')}}</a></span>
         <span class="footer__web24">|  @lang('global.projectAndRealization')
             <a href="https://web24.com.pl/" target="_blank">
-                <img src="{{ Vite::asset('resources/assets/web24logo.svg') }}" alt="Web24.com.pl">
+                <img src="{{ asset('assets/web24logo.svg') }}" alt="Web24.com.pl">
             </a>
         </span>
     </div>
