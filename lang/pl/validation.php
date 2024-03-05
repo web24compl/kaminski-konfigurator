c3a57d2585af5bec85035307b336ae3bb4ce79b8
Kacper <kacper.jakubowski.my@gmail.com>
2024-03-05 10:35:57 +0100

    Misspelling fix and translations added

--- lang/pl/validation.php
+++ lang/pl/validation.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-    "required" => "To pole jest wymagane"
+    "required" => "To pole jest wymagane",
+    "unique" => "Ten :attribute jest już zajęty",
+    "min" => [
+        "string" => ":attribute musi składać się z co najmniej :min znaków"
+    ]
 ];
